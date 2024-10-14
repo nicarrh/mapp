fcc5a26881a104638d5648c6dd6a1250aa48c39d
Nico C <nicolas.carreno@coderslab.io>
2024-10-14 10:55:58 -0300

    feat: (App) render users lists

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import useFetchUsers from '@hooks/useFetchUsers';
+import Loading from '@components/Loading';
+import Users from '@screens/Users';
 
 export default function App() {
+  const { data, error, loading } = useFetchUsers(
+    'https://jsonplaceholder.typicode.com/users',
+  );
+
+  const renderError = () => (
+    <View style={styles.container}>
+      <Text style={styles.tinyText}>{error}</Text>
+    </View>
+  );
+
+  if (error) return renderError();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {loading && !data ? <Loading /> : <Users data={data} />}
     </View>
   );
 }
@@ -13,8 +28,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  tinyText: {
+    color: '#666',
+    fontSize: 14,
   },
 });
